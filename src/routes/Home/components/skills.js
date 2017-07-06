5117f9abdfc91022dfa3084daf03812fa09acefe
Tejashree Kate <teju.kate@gmail.com>
2017-07-06 19:40:25 -0400

    changes

--- src/routes/Home/components/skills.js
+++ src/routes/Home/components/skills.js
@@ -17,10 +17,10 @@ export default class Skills extends React.Component{
 			<div className="container-fluid skillsSection">
 				<p className="sectionTitle">Skills</p>
 				<div className="chartDiv">
-					<p className="skillsText">Hi, I'm Tejashree with a professional experinece of around 4 years. I have successfully delivered
-					multiple projects from web to mobile applications, seving diffrent roles and responsibilites for my organization. 
-					I have a diverse skill set from design to development. I also got a chance to extend my knowledge to Data Analytics through my 
-					learnings from Masters program. You can expect hard work, sincerity, dedication, good learner from me and ofcourse a wow factor in my work.
+					<p className="skillsText">Hi, I'm Tejashree with a professional experience of around 4 years. I have successfully delivered
+					multiple projects from web to mobile, serving different roles and responsibilites at my organization. 
+					I have a diverse skill set ranging from design to development. I also got a chance to extend my knowledge to Data Analytics through my 
+					learnings at the Masters program. You can expect hard work, sincerity, dedication, quick turnaround time from me and ofcourse a wow factor in my work.
 					 If your are looking to hire, you can get my contact details here.
 					</p>
 					<div className="row">
