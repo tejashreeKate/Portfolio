c26312b3087ce20d5744ced855c180840a2a3015
Tejashree Kate <teju.kate@gmail.com>
2017-07-10 22:28:45 -0400

    updating work

--- src/routes/Home/components/work.js
+++ src/routes/Home/components/work.js
@@ -10,23 +10,43 @@ export default class Work extends React.Component{
 				<p className="sectionTitle">Projects</p>
 				<p className="subTitle">Academic Projects</p>
 				<div className="row projectContainer">
-					<div className="col-md-4 col-sm-4">
-						<a href="https://web.njit.edu/~tpk22/iCourseAssist/frame.html"><div className="projectDiv">iCourseFinder App Design</div></a>
+					<div className="col-md-3 col-sm-3">
+						<a href="https://web.njit.edu/~tpk22/iCourseAssist/frame.html">
+							<div className="projectDiv"><i className="fa fa-2x fa-link"></i><br/>
+							iCourseFinder App Design</div>
+							</a>
 					</div>
-					<div className="col-md-4 col-sm-4">
-						<a href="https://public.tableau.com/views/FinalpptWithFeedback_0/Sheet1World?:embed=y&:display_count=yes"><div className="projectDiv">Data Visulaisation (Tableau)</div></a>
+					<div className="col-md-3 col-sm-3">
+						<a href="https://public.tableau.com/views/FinalpptWithFeedback_0/Sheet1World?:embed=y&:display_count=yes">
+						<div className="projectDiv"><i className="fa fa-2x fa-bar-chart"></i><br/>
+						Data Visulaisation (Tableau)</div></a>
 					</div>
-					<div className="col-md-4 col-sm-4"></div>
+					<div className="col-md-3 col-sm-3">
+						<a href="https://github.com/tejashreeKate/SparseMatrix-DataType">
+							<div className="projectDiv"><i className="fa fa-2x fa-github-square"></i><br/>
+							Sparse Matrix Data Type</div>
+						</a>
+					</div>
+					<div className="col-md-3 col-sm-3">
+						<a href="https://github.com/tejashreeKate/SentimentAnalysis">
+							<div className="projectDiv"><i className="fa fa-2x fa-github-square"></i><br/>
+							Sentiment <br/>Analysis</div>
+						</a>
+					</div>
 				</div>
 				<p className="subTitle">Personal Projects</p>
 				<div className="row projectContainer">
-					<div className="col-md-4 col-sm-4">
-						<div className="projectDiv1" onClick={this.showPage.bind(this,'resume')}>Online Portfolio</div>
+					<div className="col-md-3 col-sm-3">
+						<div className="projectDiv1" onClick={this.showPage.bind(this,'resume')}>
+						<i className="fa fa-2x fa-globe"></i><br/>
+						Online Portfolio</div>
 					</div>
-					<div className="col-md-4 col-sm-4">
-						<div className="projectDiv1" onClick={this.showPage.bind(this,'paintings')}>Check out some of my paintings</div>
+					<div className="col-md-3 col-sm-3">
+						<div className="projectDiv1" onClick={this.showPage.bind(this,'paintings')}>
+						<i className="fa fa-2x fa-paint-brush"></i><br/>
+						My paintings</div>
 					</div>
-					<div className="col-md-4 col-sm-4">
+					<div className="col-md-3 col-sm-3">
 {/*						<div className="projectDiv1">Design Mockups</div>
 */}					</div>
 				</div>
