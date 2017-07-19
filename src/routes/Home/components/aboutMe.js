ee6e4902ff3e69a7aaab1e1f03f00bac3ec1e672
Tejashree Kate <teju.kate@gmail.com>
2017-07-19 15:04:36 -0400

    updates

---
+++ src/routes/Home/components/aboutMe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class About extends React.Component{
+	render(){
+		return(
+			<div className="container-fluid aboutSection" id="whatIDo">
+				<p className="sectionTitle">About Me</p>
+				<p className="skillsText">Hi, I'm Tejashree with a professional experience of around 4 years. I have successfully delivered
+					multiple projects from web to mobile, serving different roles and responsibilites at my organization. 
+					I have a diverse skill set ranging from design to development. I also got a chance to extend my knowledge to Data Analytics through my 
+					learnings at the Masters program. You can expect hard work, sincerity, dedication, quick turnaround time from me and ofcourse a wow factor in my work.
+					 If your are looking to hire, you can get my contact details here.
+				</p>
+			</div>
+		)
+	}
+}
